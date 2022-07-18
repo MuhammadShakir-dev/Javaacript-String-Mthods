6c390ba99928aea96a235c826e4c198a3ea29b6f
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:26:57 +0500

    Create indexOf&lastIndexOf().js

---
+++ indexOf&lastIndexOf().js
@@ -0,0 +1,25 @@
+/*
+
+            indexOf(); & lastIndexOf();
+
+            index of finds the index of your word. and once it will find the first matching word it will print that 
+            word and no cheacking the same word after it.
+
+
+            lastIndexOf finds the word from the last of your string.
+
+
+
+
+*/
+
+         var a = "Hello world this is Muhammad Shakir dev and he is a very nice person";
+         var b = a.indexOf("world");
+          document.write(b);
+
+
+
+         var a = "Hello world this is Muhammad Shakir dev and he is a very nice person";
+         var b = a.lastIndexOf("is");
+          document.write(b);
+ 
