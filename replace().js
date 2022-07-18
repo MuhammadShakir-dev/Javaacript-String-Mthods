5f9de55dcbbd5ea17c06c7bf258eb893a8b42145
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:29:52 +0500

    Create replace().js

---
+++ replace().js
@@ -0,0 +1,13 @@
+/*
+
+         replace();
+         repalce simply repalce the word with the new word that you have given it.
+
+
+
+
+
+*/
+         var a = "Hello world this is Muhammad Shakir dev and he is a very nice person";
+         var b = a.replace("he","This");
+          document.write(b);
