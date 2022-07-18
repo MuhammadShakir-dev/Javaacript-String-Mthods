451fe61d169729a5bc25d8ce9d0e6587b13c5e1a
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 15:47:59 +0500

    Create Lenght_string.js

---
+++ Lenght_string.js
@@ -0,0 +1,12 @@
+ <script>
+        /*
+               Lenght property in string
+               lenght property defines the lenght of the string. or in other words its tell you
+               no of letters including spaces.
+
+        */
+
+        var a = prompt("Enter a Sentence");
+        var b = a.length;
+        alert("The lenght of the above string is : " + b)
+    </script>
