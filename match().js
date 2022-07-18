41b53ff2f15fb9c9a387ad7f49df7be78937529c
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:21:27 +0500

    Create match().js

---
+++ match().js
@@ -0,0 +1,16 @@
+/*
+            match();
+
+             match is use to find word or any thing else in the the string and it find the word throughout the string
+             and make an array for it.
+             if the repeated many times then it will show it the same times it apperes in the string.
+
+
+
+
+*/
+
+
+      var a = "Hello world this is Muhammad Shakir dev and he is a very nice person";
+      var b = a.match(/is/g);
+      document.write(b);
