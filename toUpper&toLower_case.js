7f6345fee4179c2861e78bfe790e5c454016302c
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 15:58:55 +0500

    Create toUpper&toLower_case.js

---
+++ toUpper&toLower_case.js
@@ -0,0 +1,9 @@
+  var a =prompt("Enter a Sentence");
+        var b = parseInt(prompt("Select 1 for Upper case and Select 2 for Lower case"));
+        var c = a.toLowerCase();
+        var d = a.toUpperCase();
+        if(b == 1){
+            alert(d);
+        }else{
+            alert(c);
+        }
