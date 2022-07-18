47fd1e91718c044df722e01d9e2a79c8e9d788d9
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:36:45 +0500

    Create trim().js

---
+++ trim().js
@@ -0,0 +1,9 @@
+/*
+        trim();
+        trim removes the extra white spaces from the left and right part of your string.
+
+*/ 
+         var a = "    Hello world";
+         var b = a.trim();
+          alert(b);
+  
