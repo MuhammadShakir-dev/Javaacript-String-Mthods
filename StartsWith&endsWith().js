eb560fe51ec17f6c7c4828963ad4e4fffe03afca
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:11:00 +0500

    Create StartsWith&endsWith().js

---
+++ StartsWith&endsWith().js
@@ -0,0 +1,16 @@
+startWith() 
+are use to find the first word of The string.
+
+endsWith()
+are use to find the last word of the string.
+
+
+*/
+
+      var a = "Hello world";
+      var b = a.startsWith("Hello");
+      document.write(b);
+
+      var a = "Hello world";
+      var b = a.endssWith("world");
+      document.write(b);
