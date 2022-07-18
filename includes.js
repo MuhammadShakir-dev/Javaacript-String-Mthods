e482d9bde94f4ed3a699921dd47363a4ead1dbf2
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 16:03:43 +0500

    Create includes.js

---
+++ includes.js
@@ -0,0 +1,8 @@
+/*
+Includes function/method are use to find something oin your string either its a number a char or a word etc.
+
+*/
+
+var a = "hello this is Muhammad Shakir";
+var b = a.includes("ad");
+document.write(b);
