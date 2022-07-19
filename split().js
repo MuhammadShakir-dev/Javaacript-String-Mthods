7567c88c701f7ca424ee0c326be53e9cfff639fe
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:49:13 +0500

    Create split().js

---
+++ split().js
@@ -0,0 +1,11 @@
+        /*
+            split();
+            split is used to split the string into an array.
+          
+
+
+        */
+          var a = "Hello World Welcome to the world of Javascript";
+          var b = a.split(" ");
+          document.write(b);
+   
