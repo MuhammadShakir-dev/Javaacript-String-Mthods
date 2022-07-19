ca98cea45f7686060240d55347a22f693117465e
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 13:09:47 +0500

    Create substr()&subString().js

---
+++ substr()&subString().js
@@ -0,0 +1,16 @@
+       /*
+                 substr(); & substring();
+                  substr is used to slice the string.
+                  substring is used to slice the string.
+
+        */                 
+                    var a = "Hello world";
+                    var b = a.substr(1,4);
+                    document.write(b + "<br><br>");
+    
+
+
+
+                    var c = "Hello world";
+                    var d = c.substring(1,4);
+                    document.write(d);
