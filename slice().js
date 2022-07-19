8a579e4822185c62262dbd288d9e06527e5af79a
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:59:25 +0500

    Create slice().js

---
+++ slice().js
@@ -0,0 +1,11 @@
+
+        /*
+                   slice();
+                    slice is used to slice the string.
+
+
+
+        */
+                     var a = "Muhammad Shakir";
+                     var b = a.slice(-6);
+                     document.write(b);
