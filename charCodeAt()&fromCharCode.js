2a5a98ca06e3fa2082ccbdba76e119ced45b24d3
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:35:35 +0500

    Create charCodeAt()&fromCharCode.js

---
+++ charCodeAt()&fromCharCode.js
@@ -0,0 +1,13 @@
+/*
+              charCodeAt(); & fromCharCode();
+              charCodeAt is used to find the charcter code at the index number you have given it.
+              fromCharCode is used to find the charcter at the index number you have given it.
+   
+  */
+            var a = "Hello world";
+               var b = a.charCodeAt(0);
+               document.write(b);
+                
+
+              var c = String.fromCharCode(97);
+              document.write(c);
