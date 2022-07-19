456480cf20ec4964ea2d1edfbd6a82828201bf0c
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:27:28 +0500

    Create charAt().js

---
+++ charAt().js
@@ -0,0 +1,8 @@
+ /*
+               charAt();
+                charAt is used to find the charcter at the index number you have given it.
+
+  */
+               var a =  "Hello world";
+               var b = a.charAt(3);
+               document.write(b);
