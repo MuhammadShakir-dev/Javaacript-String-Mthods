6121ebd66b338fb65ad348518650d627705bc613
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:54:03 +0500

    Create repeat().js

---
+++ repeat().js
@@ -0,0 +1,12 @@
+        /*
+
+                repeat();
+                repeat is used to repeat the string.
+              
+
+
+
+        */
+                     var a = "Javascript is love";
+                     var b = a.repeat(3 + "");
+                     document.write(b);
