c120232d23cac1307aeafbdbe6d4a409b2c54fd2
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 12:45:06 +0500

    Create concat().js

---
+++ concat().js
@@ -0,0 +1,11 @@
+  /*
+                cocat();
+                cocat is used to concatenate two string.
+              
+  
+
+  */
+                  var a = "Hello World";
+                  var b = "This is Muhammad Shakir";
+                  var c = a.concat(" " + b);
+                  document.write(c);
