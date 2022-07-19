252c6d80901e9ca176e6ac44d771f6d8ece9359d
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 13:15:13 +0500

    Create toString().js

---
+++ toString().js
@@ -0,0 +1,12 @@
+        /*
+                  toString();
+                  toString is used to convert the number into string.
+                  
+                    var a = 123;
+                    var b = a.toString();
+                    document.write(b);
+
+        */
+            var a = 2002 + "" + 2001;
+            var b = a.toString();
+            document.write(b);
